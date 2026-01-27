f73d94336fa5817e1b0d85a2822f6c6835748b13
Jason <jason.choo2004@gmail.com>
2026-01-27 12:39:14 +0800

    asd

--- Controllers/DbTestController.cs
+++ Controllers/DbTestController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using FirebirdSql.Data.FirebirdClient;
 using Microsoft.AspNetCore.Mvc;
 
 namespace YourNamespace.Controllers
@@ -9,43 +8,25 @@ namespace YourNamespace.Controllers
     [Route("api/[controller]")]
     public class DbTestController : ControllerBase
     {
-        // Firebird connection string using the 4 important values
-        private readonly string _connectionString = @"User=SYSDBA;Password=masterkey;Database=D:\SQLData\DB\ACC-TEST.FDB;DataSource=localhost;Port=3050;Dialect=3;Charset=UTF8;Pooling=true;MinPoolSize=1;MaxPoolSize=10;";
+        private readonly DbHelper _dbHelper;
 
-        [HttpGet("items")]
-        public IActionResult GetAllItems()
+        public DbTestController(DbHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        [HttpGet("users")]
+        public IActionResult GetAllUsers()
         {
             try
             {
-                var items = new List<object>();
-
-                using (var conn = new FbConnection(_connectionString))
-                {
-                    conn.Open();
-
-                    using (var cmd = new FbCommand("SELECT * FROM ST_ITEM_TPL", conn))
-                    {
-                        cmd.CommandTimeout = 30;
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-{
-    var row = new Dictionary<string, object>();
-    for (int i = 0; i < reader.FieldCount; i++)
-    {
-        row[reader.GetName(i)] = reader.GetValue(i);
-    }
-    items.Add(row);
-}
-                        }
-                    }
-                }
+                var users = _dbHelper.ExecuteSelect("SELECT CODE, PASSWD FROM SY_USER");
 
                 return Ok(new
                 {
                     success = true,
-                    count = items.Count,
-                    data = items
+                    count = users.Count,
+                    data = users
                 });
             }
             catch (Exception ex)
